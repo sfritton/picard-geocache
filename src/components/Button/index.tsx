637b66ddda66ecb0ab79a7f9b9519ae9c8c9ac75
Sam Fritton <sfritton94@gmail.com>
2020-11-30 19:01:45 -0600

    adding isRound prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,12 +3,13 @@ import styles from './button.module.css';
 
 interface Props {
   color?: 'orange' | 'orange-light' | 'blue';
+  isRound?: boolean;
 }
 
-const Button: React.FC<Props> = ({ children, color = 'orange' }) => {
+const Button: React.FC<Props> = ({ children, color = 'orange', isRound = false }) => {
   return (
-    <button data-color={color} className={styles.button}>
-      {children}{' '}
+    <button data-color={color} data-is-round={isRound} className={styles.button}>
+      {children}
     </button>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,12 +12,19 @@ export default function Home() {
         <h4>Even more</h4>Some stuff about Star Trek
         <div>
           <Button>Orange Button</Button>
+          <Button isRound>Orange Button</Button>
         </div>
         <div>
           <Button color="orange-light">Orange Light Button</Button>
+          <Button color="orange-light" isRound>
+            Orange Button
+          </Button>
         </div>
         <div>
           <Button color="blue">Blue Button</Button>
+          <Button color="blue" isRound>
+            Orange Button
+          </Button>
         </div>
       </Layout.Content>
     </Layout.Grid>
