77edc6936057406e5070cb0b2dc2f3dd34a885b9
Sam Fritton <sfritton94@gmail.com>
2020-12-05 12:16:05 -0600

    some responsive stuff

--- src/components/Layout/Content/index.tsx
+++ src/components/Layout/Content/index.tsx
@@ -9,8 +9,10 @@ interface Props {
 const Content: React.FC<Props> = ({ children, disableNav = false }) => {
   return (
     <div className={styles.contentBorder}>
-      {!disableNav && <Nav />}
-      <div className={styles.content}>{children}</div>
+      <div className={styles.content}>
+        {!disableNav && <Nav />}
+        {children}
+      </div>
       <div className={`${styles.borderBlock} ${styles.borderBlock1}`}></div>
       <div className={`${styles.borderBlock} ${styles.borderBlock2}`}></div>
       <div className={`${styles.borderBlock} ${styles.borderBlock3}`}></div>
