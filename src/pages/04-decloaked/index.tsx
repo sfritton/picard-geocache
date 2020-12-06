c0e1e19b065903d5d0024952ff056cdfa90767a2
Sam Fritton <sfritton94@gmail.com>
2020-12-06 16:49:14 -0600

    missed a file

---
+++ src/pages/04-decloaked/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { PageProps } from 'gatsby';
+import CodeScreen from '../../components/Klingon/KlingonLayout/CodeScreen';
+
+const Decloaked: React.FC<PageProps> = () => {
+  return <CodeScreen />;
+};
+
+export default Decloaked;
