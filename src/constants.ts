819a2cea1939240883910f4342cf20603ffffc3c
Sam Fritton <Samuel.Fritton@target.com>
2020-12-18 09:51:49 -0600

    fixing osprey typo

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const DISTRESS_SIGNAL_KLINGON = "malja' bopab QaptaHvIS yInchugh je. yulqa' chemvaH vIsIqnISmoH!" as const;
 export const DISTRESS_SIGNAL_ENGLISH = 'Engines damaged. Life support failing. Request immediate assistance!' as const;
-export const DISTRESS_SIGNAL_ENCODED = '↓↑	↑↑	↑→	↓←	↑→	↓←	←←	↑↔	↑↕	↑→	→↑	↑←	→→	→↑	→→	↓←	→↓	←↔	→↕	↔←	←→	↑↕	←←	↓←	↓←	↓↔	←←	→↕	↓←	↑↑	↑→	→↓	↓↑	→↕	←↔	←←	↓↑	←←	←→	←→	→→	→↕	←↓	↑→	→↑	↑←	↑↕	↔←	→↑	↓→	→↔	→↑	←↔	↑↑	↕↑	↕↑	↕↓	↔↔' as const;
+export const DISTRESS_SIGNAL_ENCODED = '↓↑	↑↑	↑→	↓←	↑→	↓←	←←	↑↔	↑↕	↑→	→↑	↑←	→→	→↑	→→	↓←	→↓	→↕	←↔	↔←	←→	↑↕	←←	↓←	↓←	↓↔	←←	→↕	↓←	↑↑	↑→	→↓	↓↑	→↕	←↔	←←	↓↑	←←	←→	←→	→→	→↕	←↓	↑→	→↑	↑←	↑↕	↔←	→↑	↓→	→↔	→↑	←↔	↑↑	↕↑	↕↑	↕↓	↔↔' as const;
 
 export const ACCESS_CODE_HINT = [
   [87, 43], // g
